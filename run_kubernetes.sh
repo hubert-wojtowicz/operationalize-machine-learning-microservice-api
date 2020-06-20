b69c494bed66f7b690c9dedc05427fbddb88696c
Hubert Wójtowicz <hubert.karol.wojtowicz@gmail.com>
2020-06-20 21:25:33 +0000

    add: script creating deployment and exposing app

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,20 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+login="hubertos"
+appName="prediction-app"
+dockerpath="$login/$appName"
+deploymentName="$appName-deploy"
+exposePort=80
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment $deploymentName --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl expose deployment/$deploymentName --type="NodePort" --port $exposePort
